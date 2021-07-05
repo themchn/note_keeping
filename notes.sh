e035d1b7e47f5e37ca652b4700770da7eaa8796b
themchn <themachine@thegnet.tk>
2021-07-05 16:02:43 -0700

    Added fzf and ripgrep requirements Using fzf and ripgrep to provide interactive search of both title and body of notes. Search and create case wrapped into default notes command. Changed delete option to use note_search function. Other misc changes.

--- notes.sh
+++ notes.sh
@@ -1,23 +1,45 @@
 #!/bin/bash
 
-# TODO single word switches to create, delete, and search notes
-# TODO tab completion of all note filenames
-note_title="$1"
-note_name=""$note_title".md"
-notes_dir=""$HOME"/gitjournal"
-
-if [ -z "$note_title" ] ; then
-    echo "Please provide note name."
-    exit 1
+# dependency check
+if which fzf > /dev/null ; then
+	:
+else
+	echo "fzf not found in PATH. Please install fzf."
+	exit 1
+fi
+if which rg > /dev/null ; then
+	:
+else
+	echo "rg not found in PATH. Please install ripgrep."
+	exit 1
 fi
 
+# defaults
+notes_dir=""$HOME"/gitjournal"
+
+# functions
+edit_note() {
 cd "$notes_dir"
+# define note filename and title
+if [[ "${note_selection[1]}" == *.md ]] ; then
+	note_name="${note_selection[1]}"
+	note_title="$(echo "${note_selection[1]}" | cut -d'.' -f1)"
+else
+	note_name=""${note_selection[0]}".md"
+	note_title="${note_selection[0]}"	
+fi
+# if note doesn't exist create it, else edit existing
 if [ -f "$note_name" ] ; then
     old_mod_time=$(stat -c %y "$note_name")
-    vim "$note_name"
+	# start vim cursor at fzf_query if set
+	if [[ -z "$fzf_query" ]] ; then
+	    vim "$note_name"
+	else
+		vim +/"$fzf_query" "$note_name"
+	fi
     new_mod_time=$(stat -c %y "$note_name")
     if [ old_mod_time != new_mod_time ] ; then
-        sed "3s|.*|modified: $(date "+%Y-%m-%dT%H:%M:%S%:z")|g" "$note_name"
+        sed -i "3s|.*|modified: $(date "+%Y-%m-%dT%H:%M:%S%:z")|g" "$note_name"
     fi
 else
     # write note to /tmp initially to avoid unnecessary incron invocations
@@ -30,7 +52,60 @@ modified: ${date}
 
 # ${note_title}
 EOF
-    vim /tmp/"$note_name"
-    sed "3s|.*|modified: $(date "+%Y-%m-%dT%H:%M:%S%:z")|g" /tmp/"$note_name"
+    vim -c "set nohlsearch" +/"$note_title" /tmp/"$note_name"
+    sed -i "3s|.*|modified: $(date "+%Y-%m-%dT%H:%M:%S%:z")|g" /tmp/"$note_name"
     mv /tmp/"$note_name" "$notes_dir"/"$note_name"
 fi
+}
+
+delete_note() {
+cd "$notes_dir"
+for note in "${note_selection[@]}" ; do
+	if [[ -f "$note" ]] ; then
+		rm -i "$note"
+	else
+		:
+	fi
+done
+}
+
+search_notes() {
+cd "$notes_dir"
+RG_DEFAULT_COMMAND="rg -i -l"
+FZF_DEFAULT_COMMAND="rg --files" fzf \
+  -m \
+  -e \
+  --ansi \
+  --disabled \
+  --reverse \
+  --print-query \
+  --bind "change:reload:$RG_DEFAULT_COMMAND {q} || true" \
+  --preview "rg -i --pretty --context 2 {q} {}"
+}
+
+case "$1" in
+    "")
+		readarray -t note_selection < <(search_notes)
+		fzf_query="${note_selection[0]}"
+		if [ -z "${note_selection[*]}" ] ; then
+			exit 1
+		fi
+		if [ "${#note_selection[@]}" -gt 2 ] ; then
+			echo "Error: Opening multiple notes not supported"
+			exit 1
+		else
+			:
+		fi
+        edit_note
+        ;;
+    delete)
+		readarray -t note_selection < <(search_notes)
+		if [ -z "${note_selection[*]}" ] ; then
+			exit 1
+		fi
+        delete_note
+        ;;
+    *)
+        echo "Error: Invalid option."
+        ;;
+esac
